08f7360c6330c6117e4dcfc5fdd59c5256386128
Faizan980 <116577420+Faizan980@users.noreply.github.com>
2022-11-06 19:56:03 +0500

    This is a BMI Calculator

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+
         function bmi(){
-            let name = document.querySelector('#name').value;
-            let height = document.querySelector('#height').value;
-            let weight = document.querySelector('#weight').value;
-            let result = (weight/height/height)*1000;
-            document.querySelector('#result').innerHTML = `${name} your BMI is ${result}`;
-        }
+            const name = document.querySelector('#name').value;
+            const height = document.querySelector('#height').value;
+            const weight = document.querySelector('#weight').value;
+            let height_status=false, weight_status=false;
+
+            if(height === '' || isNaN(height) || (height <= 0)){
+                document.getElementById('height_error').innerHTML = 'Please provide a valid height';
+            }else{
+                document.getElementById('height_error').innerHTML = '';
+                height_status=true;
+            }
+
+            if(weight === '' || isNaN(weight) || (weight <= 0)){
+                document.getElementById('weight_error').innerHTML = 'Please provide a valid weight';
+            }else{
+                document.getElementById('weight_error').innerHTML = '';
+                weight_status=true;
+            }
+
+            if(height_status && weight_status){
+                const bmi = (weight / ((height*height)/10000)).toFixed(2);
+
+                if(bmi < 18.6){
+                    result.innerHTML = name + ' you are Under weight BMI : ' + bmi;
+                }else if(bmi >= 18.6 && bmi < 24.9){
+                    result.innerHTML = name + ' you are Normal BMI : ' + bmi;
+                }else{
+                    result.innerHTML = name + ' you are Over weight BMI : ' + bmi;
+                }
+            }else{
+                alert('The form has errors');
+                result.innerHTML = '';
+            }
+        };
+            
+        
